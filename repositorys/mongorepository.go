fec4e10514e76aa7e28bbe3bc80602c75d98f1b2
Frederic <frederic.lueddecke@ese.de>
2020-11-04 14:55:03 +0100

    Init go.mod and change path of mongodb libary

--- repositorys/mongorepository.go
+++ repositorys/mongorepository.go
@@ -9,11 +9,11 @@ import (
 
 	"github.com/andromedha/golang-testapi/dataclasses"
 
-	"github.com/mongodb/mongo-go-driver/bson"
+	"go.mongodb.org/mongo-driver/bson"
 
-	"github.com/mongodb/mongo-go-driver/mongo"
-	"github.com/mongodb/mongo-go-driver/mongo/options"
-	"github.com/mongodb/mongo-go-driver/mongo/readpref"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 //MongoRepository inherited from Repository
